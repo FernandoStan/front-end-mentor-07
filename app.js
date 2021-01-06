445aed966a1417773db42efe5ccf3a3b94fd4ccb
Fernando Vieira <67053077+FernandoStan@users.noreply.github.com>
2021-01-06 06:02:17 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function troca() {
+    var troca = document.getElementById('troca');
+    var strong = document.getElementById('strong');
+    var strong2 = document.getElementById('strong2');
+    var strong3 = document.getElementById('strong3');
+
+    troca.classList.toggle('active');
+
+    strong.classList.toggle('mostrar');
+    strong2.classList.toggle('mostrar2');
+    strong3.classList.toggle('mostrar3')
+}
+
+console.log('follow me :), https://twitter.com/dev_fernando_');
